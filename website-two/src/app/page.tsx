2fb84d71ddb631124dbced732efd87c9418884e0
brian419 <json10@crimson.ua.edu>
2024-07-16 12:08:22 -0500

    Updating website two, just to check if i can merge with main correctly

--- website-two/src/app/page.tsx
+++ website-two/src/app/page.tsx
@@ -3,7 +3,9 @@
 export default function Home() {
   return (
     <main>
-      Website two: Chat Application
+      <h1 className='text-center text-2xl'>
+        Website two: Chat Application
+      </h1>
     </main>
   );
 }
